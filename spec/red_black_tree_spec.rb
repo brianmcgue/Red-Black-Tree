592c38ded2c0401e1ff57b4668c91de6b2796e04
Brian McGue <mcgue.brian@gmail.com>
2014-02-24 11:52:31 -0800

    Small additions to spec files.

--- spec/red_black_tree_spec.rb
+++ spec/red_black_tree_spec.rb
@@ -11,4 +11,10 @@ describe "#node" do
   it "initializes color to red" do
     n1.color.should == :red
   end
+  
+  it "initializes parent, left, and right to nil" do
+    n1.parent.should be_nil
+    n1.left.should be_nil
+    n1.right.should be_nil
+  end
 end
