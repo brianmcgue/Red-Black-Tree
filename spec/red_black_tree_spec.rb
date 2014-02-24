402ee27fbaf8f1f20d25594aec92dc7f6202d8cc
Brian McGue <mcgue.brian@gmail.com>
2014-02-24 12:24:15 -0800

    Adding RBTree to specs.

--- spec/red_black_tree_spec.rb
+++ spec/red_black_tree_spec.rb
@@ -2,39 +2,31 @@ require 'rspec'
 require 'red_black_tree'
 
 describe RBNode do
-  context "on initialize" do
+  describe "#initialize" do
     let(:n1) { RBNode.new(1) }
   
-    it "initializes value" do
+    it "sets value" do
       n1.value.should == 1
     end
   
-    it "initializes color to red" do
+    it "sets color to red" do
       n1.color.should == :red
     end
   
-    it "initializes parent, left, and right to nil" do
+    it "sets parent, left, and right to nil" do
       n1.parent.should be_nil
       n1.left.should be_nil
       n1.right.should be_nil
     end
   end
-  
-  context "within the family" do
-    let(:n1) { RBNode.new(1) }
-    let(:n2) { RBNode.new(2) }
-    let(:n3) { RBNode.new(3) }
-    let(:n4) { RBNode.new(4) }
+end
+
+describe RBTree do
+  describe "#initialize" do
+    subject { RBTree.new }
     
-    before(:each) do
-      n1.left, n1.right, n2.parent, n3.parent = n2, n3, n1, n1
-      n2.right, n3.parent = n3, n2
-    end
-    
-    it "knows the grandparent" do
-      n1.grandparent.should be_nil
-      n2.grandparent.should be_nil
-      n3.grandparent.should == n1
+    it "sets root to nil" do
+      subject.root.should be_nil
     end
   end
 end
